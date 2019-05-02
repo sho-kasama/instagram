2d904b453192c9f472108e44c9295f4640734758
sho-kasama <sho-kasama@trunk.fm>
2019-05-02 23:11:44 +0900

    ref #18 add: post destroy like fucntion

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,12 @@ class Post < ApplicationRecord
 
     belongs_to :user
     has_many :photos, dependent: :destroy
-
-    # PostモデルとLikeモデルのアソシエーションを設定する
-    has_many :likes, -> { order(created_at: :desc )}, dependent: :destroy
+    has_many :likes, dependent: :destroy
 
     # 親子関係のある関連モデル で、親から子を作成したり保存するときに使える
     accepts_nested_attributes_for :photos
 
-
+    def liked_by(curret_user)
+      Like.find_by(user_id: curret_user.id)
+    end
 end
