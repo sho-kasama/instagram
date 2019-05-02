ba3f586e47ef3a791c722d159ca77f41ab40bab9
sho-kasama <sho-kasama@trunk.fm>
2019-05-02 14:39:08 +0900

    ref #7 add: user-profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+   
+    # 下記の三項演算子は user.profile_photoがカラムがnilでなければ、user.profile_photoを返すという意味
+
+    def avatar_url(user)
+        return user.profile_photo unless user.profile_photo.nil?
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # pagesコントローラーのhomeアクションのルーティングをトップページのルーティングとして設定する
   root 'pages#home'
+  
+  
+  # ここにusersコントローラーのshowアクションのルーティングを追加する
+  # asオプションを使うとルーティングに名前をつけることができる
+  get '/users/:id', to: 'users#show', as: 'user'
+
+
 end
